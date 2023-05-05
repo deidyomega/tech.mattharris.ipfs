74863b80f5c49b1746adad8fce52d51772c2aa53
Matthew Harris <matthewharris24@gmail.com>
2023-05-05 22:50:01 +0000

    realtest

---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="ipfs://bafybeiapythqlxnbmhmdrbyakb5fs6gt3pkgvwwgh37thmomhwm7xhxvk4/Screenshot_20210213-023316.png"
+    height="400"
+  />
+</template>
+
+<script setup>
+// import HelloWorld from "@/components/HelloWorld.vue";
+</script>
