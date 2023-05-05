42c3750d963653b62f507e26b398fd731c3c2d2a
Matthew Harris <matthewharris24@gmail.com>
2023-05-05 22:58:32 +0000

    vue is too helpful

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,14 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="ipfs://bafybeiapythqlxnbmhmdrbyakb5fs6gt3pkgvwwgh37thmomhwm7xhxvk4/Screenshot_20210213-023316.png"
-    height="400"
-  />
+  <img alt="Vue logo" :src="src" height="400" />
 </template>
 
 <script setup>
-// import HelloWorld from "@/components/HelloWorld.vue";
+import { ref, onMounted } from "vue";
+
+const src = ref("");
+
+onMounted(() => {
+  src.value =
+    "ipfs://bafybeiapythqlxnbmhmdrbyakb5fs6gt3pkgvwwgh37thmomhwm7xhxvk4/Screenshot_20210213-023316.png";
+});
 </script>
